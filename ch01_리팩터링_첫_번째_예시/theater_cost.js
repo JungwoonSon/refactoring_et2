ccc268b269f530353f44319bc9f365383ce4dc03
jungwoon727 <jungwoon727@gmail.com>
2022-08-19 17:39:58 +0900

    ch01 : 함수 선언 바꾸기

--- ch01_리팩터링_첫_번째_예시/theater_cost.js
+++ ch01_리팩터링_첫_번째_예시/theater_cost.js
@@ -42,13 +42,13 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 
@@ -86,11 +86,11 @@ function statement(invoice, plays) {
     return volumeCredits;
   }
 
-  function format(aNumber) {
+  function usd(aNumber) {
     new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       minimuumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
 }
