f9323c58ceb549e45e4c4f01af9ff87bbf515368
jungwoon727 <jungwoon727@gmail.com>
2022-08-21 14:04:51 +0900

    ch01 : 변수 인라인, 함수 이름 바꾸기

--- ch01_리팩터링_첫_번째_예시/theater_cost.js
+++ ch01_리팩터링_첫_번째_예시/theater_cost.js
@@ -7,9 +7,8 @@ function statement(invoice, plays) {
       perf.audience
     }석)\n`;
   }
-  let totalAmount = appleSauce();
 
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 
@@ -63,7 +62,7 @@ function statement(invoice, plays) {
     return volumeCredits;
   }
 
-  function appleSauce() {
+  function totalAmount() {
     let totalAmount = 0;
     for (let perf of invoice.performances) {
       totalAmount += amountFor(perf);
