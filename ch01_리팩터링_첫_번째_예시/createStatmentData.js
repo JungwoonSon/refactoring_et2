279d1881175aad0dad5a1f24930bd23f8c98f94f
jungwoon727 <jungwoon727@gmail.com>
2022-08-21 17:10:06 +0900

    ch01 : amountFor 함수 클래스로 이동

--- ch01_리팩터링_첫_번째_예시/createStatmentData.js
+++ ch01_리팩터링_첫_번째_예시/createStatmentData.js
@@ -3,6 +3,28 @@ class PerformanceCalculator {
     this.performance = aPerformance;
     this.play = aPlay;
   }
+
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case 'tragedy': // 비극
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case 'comedy': // 희극
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${this.performance.play.type}`);
+    }
+
+    return result;
+  }
 }
 
 function createStatementData(invoice, plays) {
@@ -20,35 +42,13 @@ function createStatementData(invoice, plays) {
     );
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case 'tragedy': // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-
-    return result;
   }
 
   function volumeCreditsFor(aPerformance) {
