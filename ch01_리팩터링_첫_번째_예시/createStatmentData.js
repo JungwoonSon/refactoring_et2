955a4631f06fccb6e6acaff664a33d3d0b6601f8
jungwoon727 <jungwoon727@gmail.com>
2022-08-21 18:11:17 +0900

    ch01 : volumeCredits 메서드 다형성 적용

--- ch01_리팩터링_첫_번째_예시/createStatmentData.js
+++ ch01_리팩터링_첫_번째_예시/createStatmentData.js
@@ -20,11 +20,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ('comedy' === this.play.type)
-      result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
@@ -45,6 +41,10 @@ class ComedyCalculator extends PerformanceCalculator {
       result += 10000 + 500 * (this.performance.audience - 20);
     }
     return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
 
