c64201805fb99e1ffe894e018c18891d5c704591
jungwoon727 <jungwoon727@gmail.com>
2022-08-29 23:43:09 +0900

    ch04 : beforeEach 사용

--- ch04_테스트_구축하기/test/province.test.js
+++ ch04_테스트_구축하기/test/province.test.js
@@ -2,13 +2,17 @@ const Province = require("../province")
 const sampleProvinceData = require("../sample");
 
 describe('province', () => {
+  let asia;
+
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
+
   it('shortfall', () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortFall).toBe(5);
   });
 
   it('profit', () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).toBe(230);
-  })
+  });
 });
