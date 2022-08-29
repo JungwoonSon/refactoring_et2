f753ff0ee3880bfd9cf30d72566808ccf1faeae4
jungwoon727 <jungwoon727@gmail.com>
2022-08-29 23:55:39 +0900

    ch04 : change production 테스트 작성

--- ch04_테스트_구축하기/test/province.test.js
+++ ch04_테스트_구축하기/test/province.test.js
@@ -15,4 +15,10 @@ describe('province', () => {
   it('profit', () => {
     expect(asia.profit).toBe(230);
   });
+
+  it('change production', () => {
+    asia.producers[0].production = 20;
+    expect(asia.shortFall).toBe(-6);
+    expect(asia.profit).toBe(292);
+  })
 });
