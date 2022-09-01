255a87ae30e35a905a7340bf43362cb989b10ee0
jungwoon727 <jungwoon727@gmail.com>
2022-09-01 22:25:17 +0900

    ch04 : 컬렉션이 비었을 때 확인 테스트

--- ch04_테스트_구축하기/test/province.test.js
+++ ch04_테스트_구축하기/test/province.test.js
@@ -22,3 +22,25 @@ describe('province', () => {
     expect(asia.profit).toBe(292);
   })
 });
+
+describe('no producers', () => {
+  let noProducers;
+  beforeEach(() => {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20
+    };
+    noProducers = new Province(data);
+  });
+
+  it('shortfall', () => {
+    expect(noProducers.shortFall).toBe(30);
+  });
+
+  it('profit', () => {
+    expect(noProducers.profit).toBe(0);
+  });
+
+});
