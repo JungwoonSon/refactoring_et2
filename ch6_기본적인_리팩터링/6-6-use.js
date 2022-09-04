503bcce6247b8a05cc0d4deadb5aad7ebbfed824
jungwoon727 <jungwoon727@gmail.com>
2022-09-04 21:51:22 +0900

    ch06 : 변수 캡슐화하기

--- ch6_기본적인_리팩터링/6-6-use.js
+++ ch6_기본적인_리팩터링/6-6-use.js
@@ -1,6 +1,5 @@
 import { getDefaultOwner } from './6-6.js';
 
 const owner = getDefaultOwner();
-owner.firstName = 'hong';
-console.log(owner);
+console.log(owner.firstName);
 console.log(getDefaultOwner());
