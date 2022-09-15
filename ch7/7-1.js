0bdf84dbfd188190d4820f9f346e528dfdd3045b
jungwoon727 <jungwoon727@gmail.com>
2022-09-15 21:20:06 +0900

    ch07 : 레코드 캡슐화하기

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,6 +1,29 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+const organization = new Organization({ name: 'Acme Gooseberries', country: 'GB' });
 
+class Organization {
+  #data;
+  #name;
+  #country;
 
-organization.name = '애크미 구스베리';
+  constructor(data) {
+    this.#data = data;
+    this.#name = data.name;
+    this.#country = data.country;
+  }
+
+  get #name() {
+    return this.#name;
+  }
+
+  get #country() {
+    return this.#country;
+  }
+
+  get rawData() {
+    return { ...this.#data }; // 얉은복사, cloneDeep 을 사용하면 깊은 복사 가능
+  }
+}
+
+organization.name = '애크미 구스베리'; //
 console.log(organization.name);
 console.log(organization.country);
