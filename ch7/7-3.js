97363c4e662d1c9a40acef3208f3822716f31d77
jungwoon727 <jungwoon727@gmail.com>
2022-09-15 22:23:18 +0900

    ch07 : 기본형을 객체로 바꾸기 확장

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -1,10 +1,37 @@
 export class Order {
   constructor(data) {
-    this.priority = data.priority;
+    this.priority = new Priority(data.priority);
   }
 
   isHighPriority() {
-    return 'high' === this.priority || 'rush' === this.priority
+    return this.priority.higherThan('normal');
+  }
+}
+
+class Priority {
+  #value;
+  constructor(value) {
+    if(Priority.legalValues().includes(value)) {
+      this.#value = value;
+    } else {
+      throw new Error(`${value} is invalid for Priority`); // 생성자 안에서 에러를 던지는 건 보안에 취약하다고 한다.
+    }
+  }
+
+  get index() {
+    return Priority.legalValues().findIndex(this.#value);
+  }
+
+  equals(other) {
+    return this.index === other.index;
+  }
+
+  higherThan(other) {
+    return this.index > other.index;
+  }
+
+  static legalValues() {
+    return ['low', 'normal', 'high', 'rush'];
   }
 }
 
