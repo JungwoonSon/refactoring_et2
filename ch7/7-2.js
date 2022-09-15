5c8f9f297847ce505eefcb574803c614b3925071
jungwoon727 <jungwoon727@gmail.com>
2022-09-15 21:25:23 +0900

    ch07 : 컬렉션 캡슐화하기 예제

---
+++ ch7/7-2.js
@@ -0,0 +1,41 @@
+export class Person {
+  #name;
+  #courses;
+  constructor(name) {
+    this.#name = name;
+    this.#courses = [];
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get courses() {
+    return this.#courses;
+  }
+
+  set courses(courses) {
+    this.#courses = courses;
+  }
+}
+
+export class Course {
+  #name;
+  #isAdvanced;
+  constructor(name, isAdvanced) {
+    this.#name = name;
+    this.#isAdvanced = isAdvanced;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get isAdvanced() {
+    return this.#isAdvanced;
+  }
+}
+
+const hong = new Person('홍길동');
+hong.courses.push(new Course('리팩토링', true));
+console.log(hong.courses.length);
