8e6b80579d6d73f0a737e504f99f725fbdbcb43d
jungwoon727 <jungwoon727@gmail.com>
2022-09-16 09:06:30 +0900

    ch07 : 중개자 제거하기 예제

---
+++ ch7/7-8.js
@@ -0,0 +1,50 @@
+class Person {
+  #name;
+  #department;
+  constructor(name, department) {
+    this.#name = name;
+    this.#department = department;
+  }
+
+  get manager() {
+    return this.#department.manager;
+  }
+
+  get chargeCode() {
+    return this.#department.chargeCode;
+  }
+
+  get name() {
+    return this.#name;
+  }
+}
+
+export class Department {
+  #manager;
+  #chargeCode;
+  constructor(manager, chargeCode) {
+    this.#manager = manager;
+    this.#chargeCode = chargeCode;
+  }
+
+  get chargeCode() {
+    return this.#chargeCode;
+  }
+
+  set chargeCode(arg) {
+    this.#chargeCode = arg;
+  }
+
+  get manager() {
+    return this.#manager;
+  }
+
+  set manager(arg) {
+    this.#manager = arg;
+  }
+}
+
+const person = new Person('Tom', new Department('aManager', '999'));
+console.log(person.name);
+console.log(person.manager);
+console.log(person.chargeCode);
